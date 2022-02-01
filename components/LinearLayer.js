d642024d7bea42ca34ad21b5cc455f8fbb1cc0a4
Yahya <yahya@MacBook-Pro-4.local>
2022-02-01 16:46:22 +0100

    update

--- components/LinearLayer.js
+++ components/LinearLayer.js
@@ -1,5 +1,5 @@
-import React,{useContext} from 'react';
-import { View, StyleSheet,Image, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 import * as AppColor from './AppColor';
 import * as AppText from './AppText';
@@ -11,16 +11,16 @@ import TimeStampToDate from './TimeStampToDate';
 
 import AuthContext from '../auth/context';
 
-const ChatItem = ( {sender,message,timeStamp} ) => {
+const ChatItem = ({ sender, message, timeStamp }) => {
   const { user, setUser } = useContext(AuthContext);
 
   var imageName = "profile.png";
 
-const currentTime = TimeStampToDate(timeStamp);
+  const currentTime = TimeStampToDate(timeStamp);
 
-  if(sender === user) {
-     var chatDirection = "flex-end"
-     var chatColor = AppColor.chatR
+  if (sender === user) {
+    var chatDirection = "flex-end"
+    var chatColor = AppColor.chatR
   }
   else {
     var chatDirection = "flex-start"
@@ -29,67 +29,67 @@ const currentTime = TimeStampToDate(timeStamp);
 
 
 
-  
-return (
-<View style={styles.view}>
 
-{sender===user ?
-<TouchableOpacity style={[{alignSelf:"flex-end"},{backgroundColor:chatColor},styles.bgview]} >
-<AppText.PMediumText>{message}</AppText.PMediumText>
-<View style={styles.userdataview}>
-<Image style={styles.img} source={require('../assets/profile1.png')} />
-<AppText.LWhiteText>{currentTime}</AppText.LWhiteText>
-</View>
-</TouchableOpacity> :
-<TouchableOpacity style={[{alignSelf:"flex-start"},{backgroundColor:chatColor},styles.bgview]} >
-<AppText.PMediumText>{message}</AppText.PMediumText>
-<View style={styles.userdataview}>
-<Image style={styles.img} source={require('../assets/profile2.png')} />
-<AppText.LWhiteText>{currentTime}</AppText.LWhiteText>
-</View>
-</TouchableOpacity>
-}
+  return (
+    <View style={styles.view}>
 
-</View>
+      {sender === user ?
+        <TouchableOpacity style={[{ alignSelf: "flex-end" }, { backgroundColor: chatColor }, styles.bgview]} >
+          <AppText.PMediumText>{message}</AppText.PMediumText>
+          <View style={styles.userdataview}>
+            <Image style={styles.img} source={require('../assets/profile.png')} />
+            <AppText.LWhiteText>{currentTime}</AppText.LWhiteText>
+          </View>
+        </TouchableOpacity> :
+        <TouchableOpacity style={[{ alignSelf: "flex-start" }, { backgroundColor: chatColor }, styles.bgview]} >
+          <AppText.PMediumText>{message}</AppText.PMediumText>
+          <View style={styles.userdataview}>
+            <Image style={styles.img} source={require('../assets/profile.png')} />
+            <AppText.LWhiteText>{currentTime}</AppText.LWhiteText>
+          </View>
+        </TouchableOpacity>
+      }
 
-);
+    </View>
+
+  );
 };
 
 export default ChatItem;
 
 const styles = StyleSheet.create({
   view: {
-    flex:1,
-    marginVertical:8,
-    marginHorizontal:4,
-    margin:2,
+    flex: 1,
+    marginVertical: 8,
+    marginHorizontal: 4,
+    margin: 2,
   },
   bgview: {
-      width:'80%',
-      alignContent:'center',
-        borderRadius: 10,
-        borderBottomLeftRadius: 40,
-        borderRadius: 10,
-        padding:8,
-        paddingBottom:30,
-        margin:2
-      },
-      userdataview:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        position:'absolute',
-        bottom:-4,
-        margin:1
-      },
-      img: {
-        width: 25,
-        height: 25,
-        resizeMode: 'cover',
-        borderRadius: 15,
-        alignSelf: 'center',
-        borderColor:AppColor.PLightColor,
-        borderWidth:0.5,
-        marginRight:8,
-      },
+    width: '80%',
+    alignContent: 'center',
+    borderRadius: 10,
+    borderBottomLeftRadius: 40,
+    borderRadius: 10,
+    padding: 8,
+    paddingBottom: 30,
+    margin: 2
+  },
+  userdataview: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: -4,
+    margin: 1
+  },
+  img: {
+    width: 25,
+    height: 25,
+    resizeMode: 'cover',
+    borderRadius: 15,
+    alignSelf: 'center',
+    borderColor: AppColor.PLightColor,
+    borderWidth: 0.5,
+    marginRight: 8,
+  },
 
 })
